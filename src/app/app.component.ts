272ff12c4f128c27311aa5c90ee87518916c417c
Arnab Chakraborty <Arnab.Chakraborty@parexelcloud.onmicrosoft.com>
2018-10-04 10:49:48 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  private _title = "app";
+  public get title() {
+    return this._title;
+  }
+  public set title(value) {
+    this._title = value;
+  }
+}

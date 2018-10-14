4cef4461b882ac46489beaa8ed222cde871269c7
Arnab Chakraborty <Arnab.Chakraborty@parexelcloud.onmicrosoft.com>
2018-10-14 21:51:11 +0530

    Adding responsive tiles

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -13,7 +13,7 @@ export class RecipeService {
     new Recipe(
       "Second Test Recipe",
       "This tthe second recipe which is fake",
-      "https://assets.blog.foodnetwork.ca/imageserve/wp-content/uploads/2018/01/02143436/vegetarian-cacciatore-recipe/x.jpg",
+      "https://hips.hearstapps.com/del.h-cdn.co/assets/17/34/2048x1024/landscape-1503418862-chicken-thighs-delish.jpg?resize=1200:*",
       [new Ingredient("Vegetables", 10)]
     )
   ];
